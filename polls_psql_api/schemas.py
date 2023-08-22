f33531aad5c7164aff5ed39bfc6294d89c3ad270
Icimaf Imagenes <l1d9l9a4@gmail.com>
2023-08-22 14:51:34 -0400

    giving examples models in the schema definition

--- polls_psql_api/schemas.py
+++ polls_psql_api/schemas.py
@@ -4,6 +4,17 @@ class ChoiceBase(BaseModel):
     choice_text: str
     votes_number: int = 0
 
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "choice_text": "Here goes the choice text",
+                    "votes_number": 0,
+                }
+            ]
+        }
+    }
+
 class ChoiceCreate(ChoiceBase):
     pass
 
@@ -11,11 +22,35 @@ class Choice(ChoiceBase):
     id: int
     question_id: int
 
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "id": 1,
+                    "choice_text": "Here goes the choice text",
+                    "votes_number": 0,
+                    "question_id": 2,
+                }
+            ]
+        }
+    }
+
     class Config:
+        # for replacing the orm_mode = True
         from_attributes = True
 
 class QuestionBase(BaseModel):
     question_text: str
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "question_text": "Here goes the question text",
+                }
+            ]
+        }
+    }
 
 class QuestionCreate(QuestionBase):
     pass
@@ -24,5 +59,31 @@ class Question(QuestionBase):
     id: int
     choices: list[Choice] = []
 
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "id": 3,
+                    "question_text": "Here goes the question text",
+                    "choices": [
+                        {
+                            "id": 1,
+                            "choice_text": "Here goes the choice text",
+                            "votes_number": 0,
+                            "question_id": 3,
+                        },
+                        {
+                            "id": 2,
+                            "choice_text": "Here goes the other choice text",
+                            "votes_number": 2,
+                            "question_id": 3,
+                        },
+                    ]
+                }
+            ]
+        }
+    }
+
     class Config:
+        # for replacing the orm_mode = True
         from_attributes = True
