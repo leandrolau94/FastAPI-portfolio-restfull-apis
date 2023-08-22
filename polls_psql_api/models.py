aee262a941c5349d1302e542a6e0befa65732f46
Icimaf Imagenes <l1d9l9a4@gmail.com>
2023-08-21 23:53:14 -0400

    beginning the polls app restfull api

---
+++ polls_psql_api/models.py
@@ -0,0 +1,46 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+class Question(Base):
+    __tablename__ = "questions"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+    question_text = Column(
+        String,
+        unique=True,
+        index=True
+    )
+
+    choices = relationship(
+        "Choice",
+        back_populates="question_root"
+    )
+
+class Choice(Base):
+    __tablename__ = "choices"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+    choice_text = Column(
+        String,
+        unique=True,
+        index=True
+    )
+    question_id = Column(
+        Integer,
+        ForeignKey("questions.id")
+    )
+
+    question_root = relationship(
+        "Question",
+        back_populates="choices"
+    )
