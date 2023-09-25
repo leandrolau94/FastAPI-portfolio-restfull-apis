33a26a5ba4fbd1e6e550f406811f5d023dd2e9b4
leandrolau94 <l1d9l9a4@gmail.com>
2023-09-25 08:56:11 +0200

    Improve the frontend textfield search focus and clear logic in diagnostic app

--- diagnostic_app/frontend/src/components/SicknessDashboard.js
+++ diagnostic_app/frontend/src/components/SicknessDashboard.js
@@ -1,10 +1,12 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
 import { SicknessContext } from '../App';
 import Sickness from './Sickness';
 
 import TextField from '@mui/material/TextField';
 import List from '@mui/material/List';
 import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import ClearAllIcon from '@mui/icons-material/ClearAll';
 
 const SicknessDashboard = () => {
 
@@ -14,6 +16,8 @@ const SicknessDashboard = () => {
 
     const [searchVal, setSearchVal] = useState("");
     const [showDisease, setShowDisease] = useState(sickness);
+
+    const inputSearchRef = useRef(null);
 
     useEffect(() => {
         if (searchVal === "") {
@@ -33,14 +37,19 @@ const SicknessDashboard = () => {
         setShowDisease(symptomsFilterBySearch);
     };
 
+    const handleCleanSearch = () => {
+        setSearchVal("");
+        document.getElementById("outlined-basic").value = "";
+        inputSearchRef.current.focus();
+    };
+
     useEffect(() => {
         handleSymptomsLiveSearch();
-        // console.log(searchVal);
     }, [searchVal]);
 
   return (
     <Box sx={{ textAlign: "center", justifyContent: 'center', width: "100%", height: "100%" }}>
-        <TextField id="outlined-basic" label="Teclee los sintomas para mostrar posibles enfermedades" variant="outlined" onChange={e => setSearchVal(e.target.value)} sx={{ width: "100%", marginBottom: 3 }} />
+        <TextField id="outlined-basic" inputRef={inputSearchRef} label="Teclee los sintomas para mostrar posibles enfermedades" variant="outlined" onChange={e => setSearchVal(e.target.value)} sx={{ width: "100%", marginBottom: 3 }} />
         <List sx={{ width: '100%', bgcolor: 'background.paper', textAlign: "center", justifyContent: 'center', mx: "auto", borderLeft: "1px solid gray", borderTop: "1px solid gray", borderBottom: "1px solid gray", borderRadius: "5px", overflowY: "scroll" }}>
             {
                 showDisease.map((showDisease) => {
@@ -50,6 +59,10 @@ const SicknessDashboard = () => {
                 })
             }
         </List>
+        <Fab variant="extended" color='primary' sx={{ marginTop: 2 }} onClick={handleCleanSearch}>
+            <ClearAllIcon sx={{ mr: 1 }} />
+            clean search
+        </Fab>
     </Box>
   )
 };
