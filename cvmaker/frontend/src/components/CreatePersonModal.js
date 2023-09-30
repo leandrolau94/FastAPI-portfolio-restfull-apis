226ea584818532c093b3e2ffaac0f1af1604ec5a
leandrolau94 <l1d9l9a4@gmail.com>
2023-09-30 03:12:06 +0200

    Making the create person logic for client side in cvmaker app

--- cvmaker/frontend/src/components/CreatePersonModal.js
+++ cvmaker/frontend/src/components/CreatePersonModal.js
@@ -1,33 +1,100 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
-import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import NoteAddRoundedIcon from '@mui/icons-material/NoteAddRounded';
+
+import { PersonsContext } from '../App';
+import api from './api';
 
 const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: "90%",
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
+    justifyContext: "center",
+    textAlign: "center",
+    '& .MuiTextField-root': { m: 1, width: '25ch' },
 };
 
 const CreatePersonModal = ({open, handleClose}) => {
+
+    const personsContext = useContext(PersonsContext);
+    const newPerson = personsContext.newPerson;
+    const setNewPerson = personsContext.setNewPerson;
+    const fetchPersons = personsContext.fetchPersons;
+
+    const handlePostNewPerson = async () => {
+        await api.post(
+            "/persons/",
+            newPerson,
+            {
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                },
+                withCredentials: true,
+            }
+        ).then(response => {
+            console.log(response);
+        }).catch(err => {
+            console.log(err);
+        });
+        await fetchPersons();
+        return handleClose();
+    };
+
     return (
         <Modal aria-labelledby="transition-modal-title" aria-describedby="transition-modal-description" open={open} onClose={handleClose} closeAfterTransition slots={{ backdrop: Backdrop }} slotProps={{ backdrop: {timeout: 500,},}}>
             <Fade in={open}>
-                <Box sx={style}>
-                <Typography id="transition-modal-title" variant="h6" component="h2">
-                    Text in a modal
-                </Typography>
-                <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                </Typography>
+                <Box component="form" sx={style} noValidate autoComplete='off'>
+                    <div>
+                        <TextField
+                        id="outlined-multiline-flexible"
+                        label="Full Name"
+                        onChange={(e) => {setNewPerson({...newPerson, full_name: e.target.value})}}
+                        multiline
+                        maxRows={4}
+                        />
+                        <TextField
+                        id="outlined-multiline-flexible"
+                        label="Email"
+                        onChange={(e) => {setNewPerson({...newPerson, email: e.target.value})}}
+                        multiline
+                        maxRows={4}
+                        />
+                        <TextField
+                        id="outlined-multiline-flexible"
+                        label="Address"
+                        onChange={(e) => {setNewPerson({...newPerson, address: e.target.value})}}
+                        multiline
+                        maxRows={4}
+                        />
+                        <TextField
+                        id="outlined-multiline-flexible"
+                        label="Postal Code"
+                        onChange={(e) => {setNewPerson({...newPerson, postal_code: parseInt(e.target.value)})}}
+                        multiline
+                        maxRows={4}
+                        />
+                        <TextField
+                        id="outlined-multiline-flexible"
+                        label="Phone Number"
+                        onChange={(e) => {setNewPerson({...newPerson, phone_number: e.target.value})}}
+                        multiline
+                        maxRows={4}
+                        />
+                        <Button variant="contained" endIcon={<NoteAddRoundedIcon />} onClick={handlePostNewPerson}>
+                            Create
+                        </Button>
+                    </div>
                 </Box>
             </Fade>
         </Modal>
